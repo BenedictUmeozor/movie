d0425621ec43794078f070cdf8a23a11f6df6577
Benedict <benedictumeozor@gmail.com>
2024-10-01 17:59:31 +0100

    changed to similar movies

--- components/movie/similar-movies.tsx
+++ components/movie/similar-movies.tsx
@@ -2,26 +2,26 @@ import { MovieResponse } from "@/types/globals";
 import Container from "../ui/container";
 import Movie from "../ui/movie";
 
-const getRecommendations = async ({
+const getSimilarMovies = async ({
   movieId,
 }: {
   movieId: string;
 }): Promise<MovieResponse> => {
   const response = await fetch(
-    `${process.env.API_BASE_URL}/movie/${movieId}/recommendations?api_key=${process.env.API_KEY}`,
+    `${process.env.API_BASE_URL}/movie/${movieId}/similar?api_key=${process.env.API_KEY}`,
   );
 
   return response.json();
 };
 
 const SimilarMovies = async ({ movieId }: { movieId: string }) => {
-  const { results } = await getRecommendations({ movieId });
+  const { results } = await getSimilarMovies({ movieId });
 
   return (
     <div className="my-12">
       <Container>
         <h3 className="mb-8 flex items-center gap-2 text-2xl font-bold leading-normal tracking-wide max-lg:text-xl">
-          Recommendations
+          Similar
         </h3>
         <div className="grid grid-cols-2 gap-2 md:grid-cols-3 lg:grid-cols-4">
           {results.slice(0, 8).map((movie) => (
