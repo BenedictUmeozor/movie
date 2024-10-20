4108dc4d9b87859e2fdb1ce31846549dbdaea153
Benedict <benedictumeozor@gmail.com>
2024-10-20 13:33:41 +0100

    footer update

--- components/ui/footer.tsx
+++ components/ui/footer.tsx
@@ -1,3 +1,4 @@
+import Link from "next/link";
 import Container from "./container";
 import { Separator } from "./separator";
 
@@ -6,8 +7,15 @@ const Footer = () => {
     <footer className="pt-4">
       <Container>
         <Separator color="#9f9f9f" />
-        <p className="py-4 text-center text-medium-white lg:text-left">
-          &copy; {new Date().getFullYear()} Movie Empire
+        <p className="py-4 text-center text-sm text-medium-white lg:text-left">
+          &copy; {new Date().getFullYear()} Movie Empire - Made by{" "}
+          <Link
+            target="_blank"
+            href={"https://benedictumeozor.vercel.app/"}
+            className="underline hover:text-primary-blue"
+          >
+            Benedict
+          </Link>
         </p>
       </Container>
     </footer>
